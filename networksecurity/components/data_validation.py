9120fdf493c9859b0ad10ebf7b1443ecf922ff83
Adegoke Idowu Thomas <ayanfeoluwadegoke@gmail.com>
2025-05-31 14:30:13 +0100

    Data Validation Implemented

--- networksecurity/components/data_validation.py
+++ networksecurity/components/data_validation.py
@@ -37,6 +37,7 @@ class DataValidation:
         except Exception as e:
             raise NetworkSecurityException(e,sys)
         
+        
     def detect_dataset_drift(self,base_df,current_df,threshold=0.05)->bool:
         try:
             status=True
