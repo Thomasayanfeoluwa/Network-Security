72024b42222304111e8f0208f694cf512998200d
Adegoke Idowu Thomas <ayanfeoluwadegoke@gmail.com>
2025-05-31 11:05:37 +0100

    Test MongoDB Adjusted

--- test_mongodb.py
+++ test_mongodb.py
@@ -2,7 +2,7 @@
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 
-uri = "mongodb+srv://ayanfeoluwadegoke:Admin5021@cluster0.oqtogab.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+uri = "mongodb+srv://ayanfeoluwadegoke:<password>@cluster0.oqtogab.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 
 # Create a new client and connect to the server
 client = MongoClient(uri, server_api=ServerApi('1'))
